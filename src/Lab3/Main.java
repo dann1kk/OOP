0bf706bd386b90f9531397025b025d01493d71eb
Daniel <daniel.pogorevici@isa.utm.md>
2021-10-18 00:07:10 +0300

    Lab3

---
+++ src/Lab3/Main.java
@@ -0,0 +1,89 @@
+package Lab3;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {  // Main class
+        String text = "Today it seems to me providential that \n" + "Fate should have chosen Braunau on the Inn as my birthplace.\n" + "For this little town lies on the boundary between two German \n" + "states which we of the younger generation at least have made it \n" + "our life work to reunite by every means at our disposal \n" + "German-Austria must return to the great German mother \n" + "country, and not because of any economic considerations. No,\n" + "and again no: even if such a union were unimportant from an \n" + "economic point of view; yes, even if it were harmful, it must \n" +
+                "nevertheless take place. One blood demands one Reich. Never \n" + "will the German nation possess the moral right to engage in \n" + "colonial politics until, at least, it embraces its own sons within a \n" + "single state. Only when the Reich borders include the very last \n" + "German, but can no longer guarantee his daily bread, will the \n" + "moral right to acquire foreign soil arise from the distress of our \n" + "own people. Their sword will become our plow, and from the \n" +
+                "tears of war the daily bread of future generations will grow. \n" + "And so this little city on the border seems to me the symbol of a \n" + "great mission. And in another respect as well, it looms as an \n" + "admonition to the present day. More than a hundred years ago,\n" + "this insignificant place had the distinction of being immortalized \n" + "in the annals at least of German history, for it was the scene of a \n" +
+                "tragic catastrophe which gripped the entire German nation. At " + "the time of our fatherland's deepest humiliation, Johannes Palm 1 \n" + "of Nuremberg, burgher, bookseller, uncompromising nationalist \n" + "and French hater, died there for the Germany which he loved so \n" + "passionately even in her misfortune. He had stubbornly refused \n" + "to denounce his accomplices, who were in fact his superiors. In \n" +
+                "this he resembled Leo Schlageter.\" And like him, he was denounced to the French by a representative of his government.\n" + "An Augsburg police chief won this unenviable fame, thus furnishing an example for our modern German officials in Herr Severing's 3 Reich.\n" + "In this little town on the Inn, gilded by the rays of German \n" + "martyrdom, Bavarian by blood, technically Austrian,' lived my \n" + "parents in the late eighties of the past century; my father a \n" +
+                "dutiful civil servant, my mother giving all her being to the household, and devoted above all to us children in eternal, loving care.\n" + "Little remains in my memory of this period, for after a few years \n" + "my father had to leave the little border city he had learned to \n" + "love, moving down the Inn to take a new position in Passau, that \n" + "is, in Germany proper. \n" + "In those days constant moving was the lot of an Austrian cus-\n" +
+                ". toms official. A short time later, my father was sent to Linz, and \n" + "there he was finally pensioned. Yet, indeed, this was not to \n" + "mean' rest' for the old gentleman. In his younger days, as the \n" + "son of a poor cottager, he couldn't bear to stay at home. Before \n" + "he was even thirteen, the little boy laced his tiny knapsack and \n" + "ran away from his home in the Waldviertel.' Despite the attempts of 'experienced' villagers to dissuade him, he made his\n" +
+                "way to Vienna, there to learn a trade. This was in the fifties of \n" + "the past century. A desperate decision, to take to the road with \n" + "only three gulden for travel money, and plunge into the unknown. By the time the thirteenyear old grew to be seventeen,\n" + "he had passed his apprentice's examination, but he was not yet \n" + "content. On the contrary. The long period of hardship, endless \n" + "misery, and suffering he had gone through strengthened his determination to give up his trade and become' something better.'\n" +
+                "Formerly the poor boy had regarded the priest as the embodiment of all humanly attainable heights; now in the big city,\n" + "which had so greatly widened his perspective, it was the rank of \n" + "civil servant. With all the tenacity of a young man whom suffering and care had made' old' while still half a child, the seventeen year-old clung to his new decision - he did enter the civil service.\n" + "And after nearly twenty-three years, I believe, he reached his \n" + "goal. Thus he seemed to have fulfilled a vow which he had made \n" +
+                "as a poor boy: that he would not return to his beloved native \n" + "village until he had made something of himself. \n" + "His goal was achieved; but no one in the village could remember \n" + "the little boy of former days, and to him the village had grown \n" + "strange. \n" +
+                "When finally, at the age of fifty-six, he went into retirement,\n" + "he could not bear to spend a single day of his leisure in idleness.\n" + "Near the Upper Austrian market village of Lambach he bought \n" + "a farm, which he worked himself, and thus, in the circuit of a \n" + "long and industrious life, returned to the origins of his forefathers.\n" + "Ii was at this time that the first ideals took shape in my breast.\n" +
+                "All my playing about in the open, the long walk to school, and \n" + "particularly my association with extremely' husky' boys, which \n" + "sometimes caused my mother bitter anguish, made me the very \n" + "opposite of a stay-at-home. And though at that time I scarcely" + "had any seriousideas as to the profession I should one day pursue \n" + "my sympathies were in any case not in the direction of my father's \n" + "career. I believe that even then my oratorical talent was being \n" + "developed in the form of more or less violent arguments with my \n" + "schoolmates. I had become a little ringleader; at schoolI learned \n" +
+                "easily and at that time very well, but was otherwise rather hard \n" + "to handle. Since in my free time I received singing lessons in the \n" + "cloister at Lambach, I had excellent opportunity to intoxicate \n" + "myself with the solemn splendor of the brilliant church festivals.\n" + "As was only natural, the abbot seemed to me, as the village \n" + "priest had once seemed to my father, the highest and most desirable ideal. For a time, at least, this was the case. But since \n" +
+                "my father, for understandable reasons, proved unable to appreciate the oratorical talents of his pugnacious boy, or to draw from \n" + "them any favorable conclusions regarding the future of his offspring, he could, it goes without saying, achieve no understanding \n" + "for such youthful ideas. With concern he observed this conflict \n" + "of nature.\n" +
+                "As it happened, my temporary aspiration for this profession \n" + "was in any case soon to vanish, making place for hopes more \n" + "suited to my temperament. Rummaging through my father's \n" + "library, I had come across various books of a military nature,\n" +
+                "among them a popular edition of the Franco-German War of \n" + "1870-71.1 It consisted of two issues of an illustrated periodical \n" + "from those years, which now became my favorite reading matter. \n" + "It was not long before the great heroic struggle had become my \n" + "greatest inner experience. From then on I became more and more \n" + "enthusiastic about everything that was in any way connected \n" +
+                "with war or, for that matter, with soldiering.\n" + "But in another respect aswell, this was to assume importance for \n" + "me. For the first time, though as yet in a confused form, the question was forced upon my consciousness: Was there a difference \n" +
+                "- and if so what difference - between the Germans who fought \n" + "these battles and other Germans? Why hadn't Austria taken \n" + "part in this war; why hadn't my father and all the others fought?";
+        System.out.println("Sentences: " + TextManipulations.Sentences(text));
+        System.out.println("Words: " + TextManipulations.Words(text));
+        var lettersCounts = TextManipulations.Letters(text);
+        System.out.println("All letters: "+lettersCounts[0]);
+        System.out.println("Vowels: "+lettersCounts[1]);
+        System.out.println("Consonants: "+lettersCounts[2]);
+        var topWords = TextManipulations.TopWords(text);
+        var topWordsList = new ArrayList(topWords.keySet());
+        System.out.println("Top Words:");
+        for(int i=0; i<=4; i++){
+            System.out.println(i+1  +". "+ topWordsList.get(i));
+        }
+        System.out.println("Longest Word: "+TextManipulations.LongestWord(text));
+    }
+}
+class TextManipulations{ // Class for text manipulations
+    public static int Sentences(String str){
+        var sentenceList = str.split("[!?.:]+");
+        return sentenceList.length;
+    }
+    public static int Words(String str){
+        var wordList = str.split("\\s+");
+        return wordList.length;
+    }
+    public static int[] Letters(String str) {
+        int vowels = 0;
+        int consonants = 0;
+        str = str.toLowerCase();
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+                vowels++;
+            else if (c >= 'a' && c <= 'z')
+                consonants++;
+        }
+        int letters = vowels + consonants;
+        return new int[]{letters, vowels, consonants};
+    }
+    public static Map<String, Integer> TopWords(String str){
+        var map = new HashMap<String, Integer>();
+        var wordList = CleanWordList(str);
+        for(var w: wordList){
+            var val = map.get(w);
+            val = (val == null) ? 1 : val+1;
+            map.put(w, val);
+        }
+        LinkedHashMap<String, Integer> reverseSortedMap = new LinkedHashMap<>();
+        map.entrySet()//sort high to top
+                .stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .forEachOrdered(x -> reverseSortedMap.put(x.getKey(), x.getValue()));
+        return reverseSortedMap;
+    }
+    public static String LongestWord(String str){
+        var wordList = CleanWordList(str);
+        String longestOne = wordList[0];
+        for (String w:wordList) {
+            if(w.length() > longestOne.length()) longestOne = w;
+        }
+        return longestOne;
+    }
+    private static String[] CleanWordList(String str){
+        return str.replaceAll("[^a-zA-Z ]", "").toLowerCase().split("\\s+");
+    }
+}
